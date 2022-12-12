361bb6a1d31557b17589d89182fbe4d40ae9442d
Нұрғиса Сағынбек <82598025+Sav4ukKrasav4uk@users.noreply.github.com>
2022-12-12 13:21:50 +0600

    Create The pattern of my codes.cpp
    Please, comments about flaws

---
+++ The pattern of my codes.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma comment(linker, "/STACK:256000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define ll long long
+#define ull unsigned long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+
+void file(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int dx[8] = {-1, 0, 1, 0, -1, -1, 1, 1};
+int dy[8] = {0, -1, 0, 1, -1, 1, -1, 1};
+
+const ll inf = 1e9 + 111;
+const ll N = 82e6 + 111;
+const ll M = 111;
+const ll mod = 1e9 + 7;
+const ll P = 29;
+
+void solve(){
+}
+
+int main(){
+    fast
+    srand(time(0));
+    //file();
+
+    ll t = 1;
+    //cin >> t;
+
+    while (t--){
+        solve();
+        cout << endl;
+    }
+}
